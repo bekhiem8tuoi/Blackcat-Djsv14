51d09bc27d7356e41aa422b3ecf33e9affa3b121
Nguyễn vinh <79277028+VinhBot@users.noreply.github.com>
2022-07-30 13:30:11 +0000

    update ready

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,12 +1,19 @@
 module.exports = ( client ) => {
     const { prefix } = require("../../config.json");
     const { Activity } = require("discord.js");
-    console.log(`Tên của bot  : ${client.user.username}`.red);
-    console.log(`Tổng số lệnh : ${client.commands.size}`.blue);
-    console.log(`Tổng số người: ${client.users.cache.size}`.green);
-    console.log(`Tổng số sever: ${client.guilds.cache.size}`.yellow);
-    console.log(`Tổng số kênh : ${client.channels.cache.size}`.green);
-    console.log("\n");
+    const Discord = require("discord.js");
+    console.table({ 
+      'Tên Bot:' : `${client.user.tag}`,
+      'Guild(s):' : `${client.guilds.cache.size} Servers` ,
+      'Channel:' : `${client.channels.cache.size}`,
+      'Watching:' : `${client.guilds.cache.reduce((a, b) => a + b?.memberCount, 0)} Thành viên` ,
+      'Prefix:' : `${prefix}` ,
+      'Commands:' : `${client.commands.size}` ,
+      'Discord.js:' : `v${Discord.version}` ,
+      'Node.js:' : `${process.version}` ,
+      'Dạng platt:' : `${process.platform} ${process.arch}` ,
+      'memory:' : `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB / ${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB`
+    });
     let statuses = [
       `${prefix}help/@botname help`,
       `Prefix ${prefix}`, 
